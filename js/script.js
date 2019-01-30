c6b28b25c07b3073085681e76efe7b9d4b06649b
Ajaya Khatiwada <khatiwadaajaya@gmail.com>
2019-01-30 11:13:43 +1100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showMenu() {
+  let x = document.getElementById("navBar");
+  let y = document.getElementById("nav-icon4");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
